9b6e57bfb5cc1bae9762b54fda15b7a6e24b8015
Joshua <106053582+thisisanenigma@users.noreply.github.com>
2023-06-06 12:04:50 +0800

    Initial Commit

---
+++ src/constant/real_experiments_bikes_hours.jl
@@ -0,0 +1,80 @@
+# dataset info http://lib.stat.cmu.edu/datasets/boston
+# dataset https://github.com/selva86/datasets/blob/master/BostonHousing.csv
+
+include("linear_merging_algorithm.jl")
+using ScikitLearn
+@sk_import tree: DecisionTreeRegressor
+
+using CSV
+original_hour_run = false
+housing2_run = false
+housing3_run = true
+
+X = Array{Array{Float64,1}}(undef,0)
+y = Array{Float64,1}(undef,0)
+
+if original_hour_run
+    f = CSV.File("hour.csv")
+    for row in f
+        push!(X, [row.hr,row.temp,row.mnth,row.windspeed,row.hum])
+        push!(y, row.cnt)
+    end
+end
+
+if housing2_run
+    f = CSV.File("housing2.csv")
+    for row in f
+        push!(X, [row.lstat, row.rm,row.dis,row.crim, row.age, row.b,row.ptratio,row.nox,row.tax, row.zn,row.indus,row.chas,row.rad])
+        push!(y, row.medv)
+    end
+end
+
+if housing3_run
+    f = CSV.File("housing3.csv")
+    for row in f
+        push!(X, [row.crim, row.lstat, row.medv])
+        push!(y, row.medv)
+    end
+end
+
+function merging(X::Array{Array{Float64,1},1}, y::Array{Float64,1}, z::Int, k::Int, n::Int)
+    stop_merge_param = k
+    levels = ceil(Int,log(2,n)+1)
+    #root, stuff = create_tree(X, y, z,levels)
+    #println(length(stuff))
+    #print_leaves(stuff)
+    leaves = fit_linear_merging(X, y, 0.0, z, levels, k, convert(Float64,stop_merge_param))
+    yhat = leaves_to_yhat(X,leaves) #reconstruct the yhat from leaves
+    return yhat, length(leaves)
+end
+
+# z=2
+z=1
+k=2
+n=length(y)
+println("num samples ", n)
+result = @timed merging(X,y,z,k,n)
+yhat_merging = result[1][1]
+num_leaves_merging = result[1][2]
+time_merging = result[2]
+err_merging = mse(yhat_merging,y)
+
+
+function cart_trial(X::Array{Array{Float64,1},1}, y::Array{Float64,1})
+    regressor = DecisionTreeRegressor(max_leaf_nodes = num_leaves_merging)
+    regressor.fit(X, y)
+    yhat_cart = regressor.predict(X)
+    return regressor, yhat_cart
+end
+
+cart_result = @timed cart_trial(X,y)
+yhat_cart = cart_result[1][2]
+cart_time = cart_result[2]
+
+err_cart =mse(yhat_cart,y)
+
+println("num leaves merging ", num_leaves_merging)
+println("mse merging ", err_merging)
+println("mse cart ", err_cart)
+println("time merging ", time_merging)
+println("time cart ", cart_time)
---
+++ src/constant/real_experiments_boston_constant.jl
@@ -0,0 +1,99 @@
+# dataset info http://lib.stat.cmu.edu/datasets/boston
+# dataset https://github.com/selva86/datasets/blob/master/BostonHousing.csv
+
+include("linear_merging_algorithm.jl")
+using ScikitLearn
+@sk_import tree: DecisionTreeRegressor
+
+using CSV
+# f = CSV.File("boston.csv")
+f = CSV.File("housing2constant.csv")
+
+X = Array{Array{Float64,1}}(undef,0)
+y = Array{Float64,1}(undef,0)
+for row in f
+    push!(X, [row.lstat, row.rm,row.dis,row.crim, row.age, row.b,row.ptratio,row.nox,row.tax, row.zn,row.indus,row.chas,row.rad])
+    push!(y, row.medv)
+end
+
+function merging(X::Array{Array{Float64,1},1}, y::Array{Float64,1}, z::Int, k::Int, n::Int, sigma::Float64)
+    stop_merge_param = k
+    levels = ceil(Int,log(2,n))
+    leaves = fit_linear_merging(X, y, sigma, z, levels, k, convert(Float64,stop_merge_param))
+    yhat = leaves_to_yhat(X,leaves) #reconstruct the yhat from leaves
+    return yhat, length(leaves)
+end
+
+function cart_trial(X::Array{Array{Float64,1},1}, y::Array{Float64,1},num_leaves_merging::Int)
+    regressor = DecisionTreeRegressor(max_leaf_nodes = num_leaves_merging)
+    regressor.fit(X, y)
+    yhat_cart = regressor.predict(X)
+    param = regressor.feature_importances_
+    return regressor, yhat_cart, param
+end
+
+function find_best(sigma_vals::Array{Float64}, k_vals::Array{Int},z::Int)
+
+    mses = Dict{String, Array{Float64, 2}}()
+    mses["cart"] = Array{Float64,2}(undef, length(sigma_vals), length(k_vals))
+    mses["merging"] = Array{Float64,2}(undef, length(sigma_vals), length(k_vals))
+
+    best_sigma = sigma_vals[1]
+    best_k = k_vals[1]
+    cur_best_dif = 100000000
+    for s=1:length(sigma_vals)
+        for kk=1:length(k_vals)
+            n=length(y)
+            result = @timed merging(X,y,z,k_vals[kk],n,sigma_vals[s])
+            yhat_merging = result[1][1]
+            num_leaves_merging = result[1][2]
+            time_merging = result[2]
+            err_merging = mse(yhat_merging,y)
+            cart_result = @timed cart_trial(X,y,num_leaves_merging)
+            yhat_cart = cart_result[1][2]
+            cart_time = cart_result[2]
+            err_cart =mse(yhat_cart,y)
+            mses["cart"][s, kk] = err_cart
+            mses["merging"][s, kk] = err_merging
+            dif = err_merging
+            if dif<cur_best_dif
+                best_k = k_vals[kk]
+                best_sigma = sigma_vals[s]
+                cur_best_dif = dif
+            end
+        end
+    end
+    return best_sigma, best_k, mses
+end
+
+z=2
+sigma_vals = [1.0,2.0,3.0,4.0,5.0, 10.0]
+k_vals = [1,2,3,4,5,6]
+sigma,k, mses = find_best(sigma_vals, k_vals,z)
+
+sigma = 4.0
+k=4
+
+println("sigma is ", sigma)
+println("k is ", k)
+
+n=length(y)
+println("num samples ", n)
+result = @timed merging(X,y,z,k,n,sigma)
+yhat_merging = result[1][1]
+num_leaves_merging = result[1][2]
+time_merging = result[2]
+err_merging = mse(yhat_merging,y)
+
+cart_result = @timed cart_trial(X,y,num_leaves_merging)
+yhat_cart = cart_result[1][2]
+cart_time = cart_result[2]
+param = cart_result[1][3]
+println(param)
+err_cart =mse(yhat_cart,y)
+
+println("num leaves merging ", num_leaves_merging)
+println("mse merging ", err_merging)
+println("mse cart ", err_cart)
+println("time merging ", time_merging)
+println("time cart ", cart_time)
